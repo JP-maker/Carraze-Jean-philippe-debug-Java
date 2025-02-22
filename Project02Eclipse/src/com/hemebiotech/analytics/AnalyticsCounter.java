348dc640c1fb70c8008b9972786cb12a86531d5a
unknown <jeanphilippecarraze@gmail.com>
2025-02-22 12:27:59 +0100

    Step 4 - Just add and use sort function

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -2,30 +2,31 @@ package com.hemebiotech.analytics;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class AnalyticsCounter {		
 	
 	public static void main(String args[]) throws Exception {
 
 		Map<String, Integer> listOfSymptoms = new HashMap<String, Integer>();
+		Map<String, Integer> listOfSymptomsSorted = new HashMap<String, Integer>();
 		
 		// read symptoms from file
 		ISymptomsWithCountReader reader = new ReadSymptomDataFromFile("Project02Eclipse/symptoms.txt");
 		// get symptoms
 		listOfSymptoms = reader.GetSymptoms();
 		
+		// sort symptoms
+		listOfSymptomsSorted = sortSymptoms(listOfSymptoms);
+		
 		ISymptomWriter writer = new WriteSymptomsInFile("result.out");
 		// write symptoms to file
-		writer.writeSymptoms(listOfSymptoms);
-		
-		
-		
-		// next generate output
-		/*
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();*/
+		writer.writeSymptoms(listOfSymptomsSorted);
+	}
+	
+	public static Map<String, Integer> sortSymptoms(Map<String, Integer> symptoms) {
+		// Sort with TreeMap
+        Map<String, Integer> sortedSymptoms = new TreeMap<>(symptoms);
+		return sortedSymptoms;
 	}
 }
